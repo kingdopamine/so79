6460edeac9ed40ce5aac96cc87f6fe67d7e84f8b
Apple <apple@Apples-MacBook-Pro.local>
2020-06-16 15:19:42 +0100

    monogram test

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,15 +2,34 @@ import React from 'react'
 
 import './Contact.css'
 
-import Navigation from '../Navigation/Navigation'
+import green from './Green denim texture.png'
+
+
 
 class Contact extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.state={
+            monogram:'SO79'
+        }
+    }
+
+    monogram(event){
+        if(event.target.value===''){
+            this.setState({monogram:'SO79'})
+        } else {
+            this.setState({monogram:event.target.value})
+        }
+        
+    }
     
     render(){
         return(
-            <div>
-                {/*<Navigation menuShow="main-menu-style" igicon="menu2" bagicon="menu21"/>*/}
-                <h1>Complete Bespoke / Contact Us</h1>
+            <div id="testheading">
+                <h1 id="test1">{this.state.monogram}</h1>
+                <h1 id="test2">{this.state.monogram}</h1>
+                <input type="text" id="monogramText" onChange={(event)=>{this.monogram(event)}}></input>
             </div>
         )
     }
